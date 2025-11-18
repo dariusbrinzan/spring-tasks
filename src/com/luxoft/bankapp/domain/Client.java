5f03f7a380cf9d73209ca5977d40d359b816e369
dariusbrinzan <brinzandarius27@gmail.com>
2025-11-18 19:37:05 +0200

    modified client & bank

--- src/com/luxoft/bankapp/domain/Client.java
+++ src/com/luxoft/bankapp/domain/Client.java
@@ -1,47 +1,68 @@
 package com.luxoft.bankapp.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Client {
-	
-	private String name;
-	private Gender gender;
-	private List<Account> accounts = new ArrayList<Account>();
 
-	public Client(String name, Gender gender) {
-		this.name = name;
-		this.gender = gender;
-	}
-	
-	public void addAccount(final Account account) {
-		accounts.add(account);
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public Gender getGender() {
-		return gender;
-	}
-	
-	public List<Account> getAccounts() {
-		return Collections.unmodifiableList(accounts);
-	}
-	
-	public String getClientGreeting() {
-		if (gender != null) {
-			return gender.getGreeting() + " " + name;
-		} else {
-			return name;
-		}
-	}
-	
-	@Override
-	public String toString() {
-		return getClientGreeting();
-	}
+    private String name;
+    private Gender gender;
+    private String city;
+    private final Set<Account> accounts = new HashSet<>();
 
+    public Client(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void addAccount(final Account account) {
+        accounts.add(account);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Set<Account> getAccounts() {
+        return Collections.unmodifiableSet(accounts);
+    }
+
+    public String getClientGreeting() {
+        if (gender != null) {
+            return gender.getGreeting() + " " + name;
+        } else {
+            return name;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getClientGreeting();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return name != null && name.equals(client.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name == null ? 0 : name.hashCode();
+    }
 }
